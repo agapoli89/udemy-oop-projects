7c63c03684343fd525dfccfc0b92223c44525002
Agnieszka Polińska <agapoli@gmail.com>
2020-12-03 18:32:29 +0100

    additional exercises

---
+++ zadania/create.js
@@ -0,0 +1,48 @@
+const prod1 = {
+    name: "tofu", 
+    price: 3.99, 
+    weight: 250,
+}
+
+const prod2 = {
+    name: "pasta", 
+    price:2.85, 
+    weight: 500,
+}
+
+const create = () => {console.log(`Produkt numer jeden to: ${prod1.name}. 
+Produkt numer dwa to: ${prod2.name}. 
+Produkty kosztują razem: ${prod1.price + prod2.price} pln.
+Produkty ważą razem: ${prod1.weight + prod2.weight} gr.`);
+};
+
+create();
+
+const currentUser = {
+    name: "Aga", 
+    surname: "Poli", 
+    email: "agapoli@gmail.com", 
+    www: "przewodniczka.wroclaw.pl", 
+    userType: 'premium',
+}
+
+const show = () => {
+    console.log('imię:' + currentUser.name);
+    console.log('nazwisko:' + currentUser.surname);
+    console.log('email:' + currentUser.email);
+    console.log('www:' + currentUser.www);
+    console.log('typ użytkownika:' + currentUser.name);
+}
+
+show();
+
+const book = {
+    title: "Pan Tadeusz", 
+    author: "Adam Mickiewicz", 
+    pageCount: 300, 
+    publisher: "Ossolineum",
+
+    showDetails() {
+        
+    }
+}
