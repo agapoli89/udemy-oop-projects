9285264b38bd4885c13dd1b4eee9ea536cf0452c
Agnieszka Polińska <agapoli@gmail.com>
2020-12-08 09:45:05 +0100

    change table to object and use the data

--- zadania/create.js
+++ zadania/create.js
@@ -249,3 +249,55 @@ console.log(text.getWordsCount('aga ma kota'));
 console.log(text.setCapitalize('aga ma kotka'));
 console.log(text.setMix('aga ma kota'));
 console.log(text.generateRandom(3));
+
+// part 6
+
+const users = [
+    //id, name, surname, email, age, value
+    [  1, "Shauna", "Bradnocke", "sbradnocke0@altervista.org", 20, 108.28] ,
+    [  2, "Mela", "Redman", "mredman1@nps.gov", 24, 267.37] ,
+    [  3, "Othelia", "Lemon", "olemon2@slashdot.org", 15, 748.06] ,
+    [  4, "Meier", "Cockell", "mcockell3@icio.us", 32, 1951.64] ,
+    [  5, "Shellysheldon", "Gronowe", "sgronowe4@cnbc.com", 16, 1040.54] ,
+    [  6, "Francisca", "Tofanini", "ftofanini5@gnu.org", 21, 1544.08] ,
+    [  7, "Cliff", "Underwood", "cunderwood6@addtoany.com", 10, 451.21] ,
+    [  8, "Caron", "Falshaw", "cfalshaw7@hugedomains.com", 27, 1968.72] ,
+    [  9, "Anitra", "Warters", "awarters8@intel.com", 12, 380.68] ,
+    [ 10, "Caitrin", "Baudrey", "cbaudrey9@ihg.com", 13, 1385.44] ,
+    [ 11, "Reginald", "Beavers", "rbeaversa@elegantthemes.com", 15, 1205.52] ,
+    [ 12, "Marleen", "Aickin", "maickinb@purevolume.com", 20, 1434.10] ,
+    [ 13, "Lisa", "MacSorley", "lmacsorleyc@ocn.ne.jp", 17, 1567.07] ,
+    [ 14, "Kimberli", "Berkeley", "kberkeleyd@merriam-webster.com", 19, 1994.97] ,
+    [ 15, "Tawnya", "Illingworth", "tillingworthe@quantcast.com", 23, 1742.64]
+];
+
+const fixData = (users) => {
+    //console.log(users);
+    const newUsers = users.map(user => user = {
+        id: user[0],
+        name: user[1],
+        surname: user[2],
+        email: user[3],
+        age: user[4],
+        cash: user[5],
+    })
+    return newUsers;   
+};
+
+const generateNewTable = (fixData) => {
+    //const {id, name, surname, email, age, cash} = fixData;
+    let cashAmount = 0;
+    let averageAge = 0;
+    const emailTable = [];
+    fixData.forEach(el => {
+        cashAmount += el.cash;
+        averageAge += el.age;
+        emailTable.push(el.email);
+    });
+    console.log(cashAmount);
+    console.log(averageAge/fixData.length);
+    console.log(emailTable);
+};
+
+generateNewTable(fixData(users));
+
